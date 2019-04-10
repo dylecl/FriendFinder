98189a5418accbdb38d26bb30665f2bd7be138dc
dylecl <dylann.leclairr@gmail.com>
2019-04-10 12:32:55 -0600

    made express static path to get images to be found

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,38 @@
 var friendsTable = [
     {
-        name: "Dylan LeCLair",
-        scores: [3,3,3,3,3,3,3,3,3,3]
+        name: "Philip J. Fry",
+        scores: [3,3,3,3,3,3,3,3,3,3],
+        pic: "<img src='./assets/images/fry.jpg' alt='Philip J Fry'></img>"
     },
     {
-        name: "tori",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        name: "The Professor",
+        scores: [1, 3, 1, 2, 3, 1, 5, 1, 1],
+        pic: "<img src='./assets/images/farnsworth.gif' alt='Hubert J Farnsworth'></img>"
     },
     {
         name: "Bender",
         scores: [1,1,1,1,1,1,1,1,1,1],
-        pic: "<img src='./data/assets/trial.jpg' alt='bender'>"
+        pic: "<img src='./assets/images/bender.png' alt='Bender'></img>"
+    },
+    {
+        name: "Amy Wong",
+        scores: [4, 3, 5, 3, 1, 5, 5, 3, 5, 4],
+        pic: "<img src='./assets/images/amy.jpeg' alt='Amy Wong of the Mars Wongs'></img>"
+    },
+    {
+        name: "Dr. Zoidberg",
+        scores: [5, 1, 3, 5, 3, 3, 4, 5, 3, 5],
+        pic: "<img src='./assets/images/zoidberg.jpeg' alt='WHY NOT ZOIDBERG'></img>"
+    },
+    {
+        name: "Turanga Leela",
+        scores: [3, 3, 2, 5, 4, 4, 5, 3, 4, 5],
+        pic: "<img src='./assets/images/leela.jpeg' alt='Captain Leela'></img>"
+    },
+    {
+        name: "Hermes Conrad",
+        scores: [3, 2, 4, 4, 5, 3, 4, 2, 2, 5],
+        pic: "<img src='./assets/images/hermes.png' alt='Hermes Conrad></img>"
     }
 ];
 
